5e52bac9aef9d77072c499fff2dad94ec3099ede
Yoko-422 <nakadayoko@hotmail.com>
2025-09-11 14:00:21 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,7 @@
 let date = new Date();
 
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
-console.log (year+'年', month+'月', day+'日');
+let year = date.getFullYear();
+let month = date.getMonth()+1;
+let day = date.getDate();
+
+console.log(year+'年'+month+'月'+day+'日');
